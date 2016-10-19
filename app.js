5fea6c07547074af8012a8a6137de2071f3fe8a4
arthurakhnoyan <arthur.akhnoyan@picsar.com>
2016-10-19 11:23:01 +0400

    First commit

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphb = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodejsapp')
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+//Init App
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphb({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// bodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+// set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect flash
+app.use(flash());
+
+//Global vars for flash messages
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Middleware for route files
+app.use('/', routes);
+app.use('/users', users);
+
+//Set port
+var port = process.env.PORT || 3030;
+app.listen(port, function () {
+    console.log('Updated : Server listening at port %d', port);
+  });
